3f781af0c82ac79f5221274ebae4591ec08c9048
yc <1422776724@qq.com>
2023-06-08 20:11:17 -0700

    update .gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yc/ros/fusion
-# Build directory: /home/yc/ros/fusion/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
